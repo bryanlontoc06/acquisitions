d562359ce4af1df24717c505124e371629fa99c8
Bryan Lontoc <bryanlontoc06@gmail.com>
2026-04-03 00:57:57 +0800

    chore: Add security headers middleware

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
+from starlette.middleware.base import RequestResponseEndpoint
 
 from . import dbModels
 from .db.database import Base, engine
@@ -33,6 +35,31 @@ async def lifespan(_app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for simplicity, adjust as needed
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+# Middleware to add security headers to all responses
+@app.middleware("http")
+async def add_security_headers(
+    request: Request, call_next: RequestResponseEndpoint
+) -> Response:
+    logger.info(f"📥 Incoming: {request.method} {request.url.path}")
+    response: Response = await call_next(request)
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-XSS-Protection"] = "1; mode=block"
+    response.headers["Strict-Transport-Security"] = (
+        "max-age=31536000; includeSubDomains"
+    )
+    logger.info(f"📤 Outgoing: Status {response.status_code}")
+    return response
+
 
 app.include_router(sample.router, prefix="/v1/sample", tags=["sample"])
 
